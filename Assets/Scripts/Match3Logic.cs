fc934449e4d6ef2145e4c127f27b461d1c55a49e
Gar <garshishka@yandex.ru>
2019-12-11 14:42:29 +0300

    go

---
+++ Assets/Scripts/Match3Logic.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Match3Logic : MonoBehaviour
+{
+    private static Match3Logic _instance;
+
+    public static Match3Logic Instance { get { return _instance; } }
+
+    [SerializeField]
+    Text MovesNumber;
+    int MovesN;
+
+    [SerializeField]
+    Text DestroyedNumber;
+    int DesN;
+
+    [SerializeField]
+    Text ComboNumber;
+    int ComboN;
+
+    public int Height = 16; //The dimension of the board
+    public int Width = 10;
+
+    public GameObject Highlighter; //Little piece that will be on the first selected tile
+
+    public GameObject[] tilePieces; //Prefab list for tile types
+
+    public Transform gameBoard;
+
+    public Node firstSel;       //these will be used for selecting which tiles to swap
+    public Node secondSel;
+    
+    public Node[,] board;        //Main array where information about our board will be stored
+
+    public bool CanClick = true; //This bool is for stopping interaction on the board when something happening
+
+    int FirstBlankX; int FirstBlankY;//This will be used for checking the board after filling up the blanks
+    int LastBlank;
+
+    private void Awake()
+    {   //Singleton config
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance == this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+     /*   if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }*/
+    }
+
+    private void Start()
+    {
+        FirstBlankX = 0;
+        FirstBlankY = 0;
+        LastBlank = 0;
+        MovesN = 0;
+        MovesNumber.text = MovesN.ToString();
+        DesN = 0;
+        DestroyedNumber.text = DesN.ToString();
+        ComboN = 0;
+        StartGame();
+    }
+
+    void StartGame()
+    {   //Filling the board at the beginning
+        board = new Node[Width, Height];        
+        for (int y =0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                int value = Random.Range(1, 6);
+                value = CheckPlacing(value, x, y); 
+                //after checking we create a new tile with the type
+               GameObject NewTile = Instantiate(tilePieces[value - 1],gameBoard);
+
+               NewTile.GetComponent<Tile>().Change(x, y, false); //and change its internal properties
+
+               board[x, y] = new Node(value,NewTile); //then we place it in our table
+            }
+        }
+    }
+
+    int CheckPlacing(int val, int x, int y) 
+    {   //Checking already placed tiles on the left and down so as not to create 3 matches on the creation
+            if(x > 1 && board[x-1,y].type == val)
+            {
+                if (board[x - 2, y].type == val)
+                {                                   //if two of the same tile types repeat
+                    val = Random.Range(1, 6);       //we change the type of created tile to some other
+                    val = CheckPlacing(val, x, y);  //and check again
+                }
+            }        
+            if (y > 1 && board[x, y-1].type == val)
+            {
+                if (board[x, y-2].type == val)
+                {
+                    val = Random.Range(1, 6);
+                    val = CheckPlacing(val, x, y);
+                }
+            }
+        return val;
+    }
+
+
+    public IEnumerator CheckForBlanks()
+    {   //Getting blank places on board into its own list
+        bool FoundBlank = false;
+        for (int y = Height-1; y > -1; y--) //going up to down
+        {
+            for (int x = 0; x < Width; x++)
+            { //and left to right
+                if (board[x, y].type == 0)
+                {   //every tile up above blanks moves down
+                    FoundBlank = true;
+                    if (y != Height - 1)
+                    {
+                        board[x, y].type = board[x, y + 1].type; //we make upper node into this node
+                        board[x, y].GO = board[x, y + 1].GO; 
+
+                        board[x, y + 1].type = 0; //we make the upper node blank
+                        board[x, y + 1].GO = null;
+
+                        board[x, y].GO.GetComponent<Tile>().Change(x, y, true); //we make internal changes in this node and move it
+                    }
+                    else
+                    { //if this is the last row, we make new tiles
+                        int value = Random.Range(1, 6);
+                        value = CheckPlacing(value, x, y);
+                        //using the same idea as when creating a new board
+                        GameObject NewTile = Instantiate(tilePieces[value - 1], gameBoard);
+                        // we place a new piece one row higher than height 
+                        NewTile.transform.localPosition = new Vector3(x * 100, (y + 1) * 100);
+                        NewTile.GetComponent<Tile>().x = x;
+                        NewTile.GetComponent<Tile>().y = y + 1;
+                        NewTile.GetComponent<Tile>().Change(x, y, true); //so it will move down from up above
+                        while (NewTile.GetComponent<Tile>().DoneMoving == false)
+                        {
+                            yield return new WaitForSeconds(0.1f);
+                        }
+                        board[x, y] = new Node(value, NewTile); //placing the new tile in the board
+                    }
+                    if (x < FirstBlankX) { FirstBlankX = x; } //finding the left-most and lowest blank the board
+                    if (y < FirstBlankX) { FirstBlankY = y; } 
+                    if (x > LastBlank) { LastBlank = x; }     //finding the x of right-most blank
+                }
+            }
+        }               
+
+        if (FoundBlank)
+        { //if we had blanks we check again for new ones
+         StartCoroutine( CheckForBlanks());
+        }
+        else
+        { //if not we check every tile in the affected area for matches
+            for (int y = FirstBlankY; y < Height - 1; y++)
+            {
+                for (int x = FirstBlankX; x < LastBlank; x++)
+                {
+                    if(board[x, y].GO.GetComponent<Tile>().Check3MatchAnBreak(x, y))
+                    { //if we had matches - we have blanks
+                        FoundBlank = true;
+                    }                    
+                }
+            }
+            if (FoundBlank) //if we got matches so we check again for blanks
+            {
+                StartCoroutine(CheckForBlanks());
+            }
+            else { CanClick =true; ComboN = 0; ComboNumber.text = " "; } //if not we can swap again
+        }
+    }
+
+    public void OneMoreMove()
+    { //Changes the number of moves
+        MovesN++;
+        MovesNumber.text = MovesN.ToString();
+    }
+    public void OneMoreDestroyed()
+    { //Changes the number of desroyed tiles
+        DesN++;
+        DestroyedNumber.text = DesN.ToString();
+    }
+
+    public IEnumerator OneMoreCombo()
+    { //Showes COMBO text and number
+        ComboNumber.transform.localScale = new Vector3(0.5f, 0.5f);
+        ComboN++;
+        if (ComboN != 1)
+        { //it's little janky but I like how it looks
+            ComboNumber.text = "COMBO x"+ ComboN;
+            ComboNumber.transform.localScale += new Vector3(1f, 1f);
+            for (int i = 0; i < 2; i++)
+            {
+                ComboNumber.transform.localScale += new Vector3(0.1f, 0.1f);
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class Node //This clas governs tiles in the logic grid
+{
+    public int type;        //This have the type of a tile
+    public GameObject GO;   //And this is a container for tile gameobject
+
+    public Node(int i, GameObject T)
+    {
+        type = i; //1 - circle, 2 - triangle, 3 - diamond, 4 - square, 5 - hex, 6 - star
+        GO = T;
+    }
+}
+
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    //showing the place on a board
+    public int x;
+    public int y;
+
+    public bool DoneMoving = true; //used to stop stuff until piece is moving
+
+    public void OnClick()
+    {   //  Checking if this is the second clicked tile and its adjacent to a highlighted tile
+        if (Match3Logic.Instance.CanClick)
+        {
+            if (x > 0 && Match3Logic.Instance.board[x - 1, y] == Match3Logic.Instance.firstSel)
+            {
+                SwapTiles("Right");
+            }
+            else
+            {
+                if (x < Match3Logic.Instance.Width - 1 && Match3Logic.Instance.board[x + 1, y] == Match3Logic.Instance.firstSel)
+                {
+                    SwapTiles("Left");
+                }
+                else
+                {
+                    if (y > 0 && Match3Logic.Instance.board[x, y - 1] == Match3Logic.Instance.firstSel)
+                    {
+                        SwapTiles("Up");
+                    }
+                    else
+                    {
+                        if (y < Match3Logic.Instance.Height - 1 && Match3Logic.Instance.board[x, y + 1] == Match3Logic.Instance.firstSel)
+                        {
+                            SwapTiles("Down");
+                        }
+                        else
+                        {
+                            NewPos(); //If this is first clicked of not an adjacent to other selected tile
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void SwapTiles(string WhereTo ) //Swapping with adajacent tile
+    {
+        Match3Logic.Instance.CanClick = false; 
+        int SwapToX = x; int SwapToY = y;
+        int SwapFromX = x; int SwapFromY = y; //Those can be changed by Change fucntion, so we save them separately
+        switch (WhereTo)
+        { //we change where to move coordinate
+            case "Right":
+                SwapToX = x - 1; 
+                break;
+            case "Left":
+                SwapToX = x + 1;
+                break;
+            case "Up":
+                SwapToY = y - 1;
+                break;
+            case "Down":
+                SwapToY = y + 1;
+                break;
+        }
+        Match3Logic.Instance.secondSel = Match3Logic.Instance.board[x, y];
+
+        Match3Logic.Instance.board[SwapToX, SwapToY].GO.GetComponent<Tile>().Change(x, y,true); //Changing the  first gameobject to reflect its new placement
+        Match3Logic.Instance.board[SwapToX, SwapToY] = Match3Logic.Instance.secondSel; //Second tile moves to new place
+        Match3Logic.Instance.board[SwapToX, SwapToY].GO.GetComponent<Tile>().Change(SwapToX, SwapToY, true); //Changing the second tile gameobject to reflect its new place
+        Match3Logic.Instance.board[SwapFromX, SwapFromY] = Match3Logic.Instance.firstSel; //Moving the first tile(that already changed GO) to new place
+
+        HideHl();
+        StartCoroutine(WaitingForMove(SwapToX, SwapToY, SwapFromX, SwapFromY)); //now we wait for moving
+    }
+    IEnumerator WaitingForMove(int SFX, int SFY, int STX, int STY)
+    {
+        while (DoneMoving == false)
+        {
+            yield return new WaitForSeconds(0.1f); //Waiting for tiles to stop moving;
+        }
+        Matching(SFX, SFY, STX, STY);
+    }
+    void Matching(int SwapFromX, int SwapFromY, int SwapToX, int SwapToY)
+    {   //checking for matches from both moved tiles
+        bool Check1 = Check3MatchAnBreak(SwapFromX, SwapFromY);
+        bool Check2 = Check3MatchAnBreak(SwapToX, SwapToY);
+        if (Check1||Check2 )
+        { //if at least one of them got something we check for blanks
+           StartCoroutine(Match3Logic.Instance.CheckForBlanks());
+        }
+        else { Match3Logic.Instance.CanClick = true; }
+        Match3Logic.Instance.OneMoreMove();
+    }
+       
+   public void Change(int nx, int ny, bool NeedMove) //Changes inside coordinates, name and postition to a new one
+    {
+        if (NeedMove)
+        {  StartCoroutine(MoveTile(nx - x, ny - y));} //for moving tiles when swapping
+        else { transform.localPosition = new Vector3(nx * 100, ny * 100); } //for when we just create new tiles
+        x = nx; y = ny;
+        transform.name = "[" + x + "," + y + "]";
+    }
+
+   IEnumerator MoveTile(int dx, int dy)
+    {
+        DoneMoving = false;
+        for (int i = 0; i < 4; i++)
+        {
+            transform.localPosition += new Vector3(dx*25,dy*25);
+            yield return new WaitForSeconds(0.02f);
+        }
+        DoneMoving = true;
+    }
+
+   public bool Check3MatchAnBreak(int fx, int fy)
+    {   //Finding all 3 matches around the tile and breaking them
+        Node[,] board = Match3Logic.Instance.board;
+        int val1 = board[fx, fy].type;
+
+        //Creating lists for matched tiles in both directions 
+        List<Node> MatchListH = new List<Node>();
+        List<Node> MatchListV = new List<Node>();
+
+        if (HorizontalCheck(MatchListH, fx, fy, board, val1))
+        {
+            BreakTiles(MatchListH);
+        }
+        //finding matches,filling up match lists and breaking them
+        if (VerticalCheck(MatchListV, fx, fy, board, val1))
+        {
+            BreakTiles(MatchListV);
+        }
+
+        if(MatchListH.Count>2|| MatchListV.Count > 2)
+        { //if we had even 1 match
+            return true;
+        }
+        else { return false; }
+    }
+
+   void BreakTiles(List<Node> MatchList)
+    {   //Breaking tiles in one row
+            foreach (Node tile in MatchList) //we destroy all of them if they are still not destroyed
+            {
+                if (tile.type != 0)
+                {
+                    tile.type = 0; //we make the type 0 so finding blanks func will know that this node is dead
+                tile.GO.GetComponent<Tile>().DestroyThatTile();
+                }
+            }
+        StartCoroutine(Match3Logic.Instance.OneMoreCombo());
+    }
+
+   public void DestroyThatTile()
+    { //just to be safe we create a function to call coroutine
+        StartCoroutine(DestroyThisTile());
+    }
+   IEnumerator DestroyThisTile()
+    {   
+        for (int i = 0; i < 5; i++)
+        {   //tile will become smaller and then destroy
+            transform.localScale -= new Vector3(0.1f, 0.1f);
+            yield return new WaitForSeconds(0.02f);
+        }
+        Match3Logic.Instance.OneMoreDestroyed();
+        Destroy(gameObject);
+    }
+
+   bool HorizontalCheck(List<Node> MatchList,int wx,int wy, Node[,] board, int val)
+    { //checking left and right from the chosen tile
+        MatchList.Add(board[wx, wy]); //we add starting tile to the matched list
+        int i = wx-1;
+        while (i>-1&& board[i, wy].type == val)
+        { //while tile types matches we go left
+            MatchList.Add(board[i, wy]);
+            i--; //and add every tile to a list
+        }
+        i = wx+1;
+        while (i < Match3Logic.Instance.Width && board[i, wy].type == val)
+        {//same but to the right
+            MatchList.Add(board[i, wy]);
+            i++;
+        }
+        if (MatchList.Count > 2) { return true; } //if we have 3 or more matched tiles - sucess
+        else { return false; }
+    }
+
+   bool VerticalCheck(List<Node> MatchList, int wx, int wy, Node[,] board, int val)
+    { //checking up and down from the chosen tile
+        MatchList.Add(board[wx, wy]);  //we add starting tile to the matched list
+        int i = wy - 1;
+        while (i > -1 && board[wx, i].type == val)
+        {  //while tile types matches we go down
+            MatchList.Add(board[wx, i]);
+            i--; //and add every tile to a list
+        }
+        i = wy + 1;
+        while (i < Match3Logic.Instance.Height && board[wx, i].type == val)
+        { //same but up
+            MatchList.Add(board[wx, i]);
+            i++;
+        }
+        if (MatchList.Count > 2) {return true; } //if we have 3 or more matched tiles - sucess
+        else { return false; }
+    }
+
+   void NewPos() //Showing the selected tile
+    {
+        Match3Logic.Instance.firstSel = Match3Logic.Instance.board[x, y];
+        var HL = GameObject.Find("HLighter").transform;
+        HL.position = transform.position;
+    }
+
+   void HideHl() //Clearing the clicked tiles state and hiding highlighter
+    {
+        Match3Logic.Instance.firstSel = null;
+        Match3Logic.Instance.secondSel = null;
+        var HL = GameObject.Find("HLighter").transform;
+        HL.position = new Vector3(-1000,-1000);
+    }      
+}
